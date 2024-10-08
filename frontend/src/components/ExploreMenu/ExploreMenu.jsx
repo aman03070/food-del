8a2776e4c856ddf91cc64baf486bc0e4ac5e7110
aman03070 <amanaj379@gmail.com>
2024-10-08 20:52:55 +0530

    add files

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react'
+import './ExploreMenu.css'
+import { StoreContext } from '../../Context/StoreContext'
+
+export const CircleIndicator = () => {
+  const { scrollYProgress } = useScroll()
+
+  return (
+    <motion.path
+      d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+      style={{ pathLength: scrollYProgress }}
+    />
+  )
+}
+
+const ExploreMenu = ({category,setCategory}) => {
+
+  const {menu_list} = useContext(StoreContext);
+  
+  return (
+    <div className='explore-menu' id='explore-menu'>
+      
+      <h1 data-text='text'>Explore our menu</h1>
+      <p className='explore-menu-text'>Choose from over <b>50</b> products across <b>5+</b>categories, including groceries, meals, stationery, and medical supplies. We collaborate with <b>6+</b> campus shops to bring everything you need right to your doorstep.
+
+Our mission is to provide fast and reliable delivery to every corner of GLA University, ensuring you have what you need in <b>30 minutes or less.</b> With <b>24/7 availability</b>, your essentials are just a few clicks away.
+
+Join <b>30+</b> satisfied students and experience the convenience of on-campus delivery, making your life at GLA University smoother, one order at a time!</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+            return (
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>  
+                    <img src={item.menu_image} className={category===item.menu_name?"active":""} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './Header.css'
+import { assets } from '../../assets/assets'
+
+
+const BannerScrollEffect = () => {
+  const bannerRef = useRef(null);
+  const oneRef = useRef(null);
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (bannerRef.current) {
+      bannerRef.current.style.right = scrollY * 1 + 'px';
+    }
+    if (oneRef.current) {
+      oneRef.current.style.left = scrollY * 2 + 'px';
+    }
+  }, [scrollY]);
+    return (
+        <div className='header'>
+            <section className='banner'>
+                <img id='banner' ref={bannerRef} src={assets.banner_img} />
+                <img id='one' ref={oneRef} src={assets.one_img} />
+            </section>
+            <div className='header-contents' id='text'>
+                <h2>Order your essentials here!!</h2>
+                <p>Browse a variety of products from your favorite campus stores, including groceries, meals, stationery, and medical supplies. Our mission is to bring convenience to every corner of GLA University, delivering what you need right to your door, quickly and efficiently.</p>
+                <button><a href='#explore-menu'>View Menu</a></button>
+            </div>
+            <hr />
+        </div>
+    )
+}
+
+export default BannerScrollEffect;
